cc4b2ad34fd5b93a553c3457da79c01c1443be79
escandellari <elisa.scandellari@gmail.com>
2023-05-05 09:24:25 +0100

    Updated the settings

--- baking/settings.py
+++ baking/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-# MEDIA_URL = "/media/"
-# STATIC_ROOT = Path(BASE_DIR / "static")
-# MEDIA_ROOT = Path(BASE_DIR / "media")
+MEDIA_URL = "/media/"
+STATIC_ROOT = Path(BASE_DIR / "static")
+MEDIA_ROOT = Path(BASE_DIR / "media")
 LOGIN_REDIRECT_URL = "/"
