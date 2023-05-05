ac07d04a22ab5d785585c46ffd0d3b755094215d
escandellari <elisa.scandellari@gmail.com>
2023-05-05 09:19:28 +0100

    Updated the settings, linting, new icons

--- baking/settings.py
+++ baking/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
-    "django_bootstrap_icons",
+    "fontawesomefree",
 ]
 
 MIDDLEWARE = [
@@ -125,6 +125,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-MEDIA_URL = "/media/"
-STATIC_ROOT = Path(BASE_DIR / "static")
-MEDIA_ROOT = Path(BASE_DIR / "media")
+# MEDIA_URL = "/media/"
+# STATIC_ROOT = Path(BASE_DIR / "static")
+# MEDIA_ROOT = Path(BASE_DIR / "media")
+LOGIN_REDIRECT_URL = "/"
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
@@ -11,6 +12,7 @@ def post_list(request):
     return render(request, "blog/post_list.html", {"posts": posts})
 
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by("created_date")
     return render(request, "blog/post_draft_list.html", {"posts": posts})
@@ -21,6 +23,7 @@ def post_detail(request, pk):
     return render(request, "blog/post_detail.html", {"post": post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -35,6 +38,7 @@ def post_new(request):
     return render(request, "blog/post_edit.html", {"form": form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -50,12 +54,14 @@ def post_edit(request, pk):
     return render(request, "blog/post_edit.html", {"form": form})
 
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect("post_detail", pk=pk)
 
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
